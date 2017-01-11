8b022219e8d1103885a4f00867ffd66c276c163d
liuaaaddxiaoer <liuxiaoer@liuxiaoerdeMacBook-Pro.local>
2017-01-11 11:43:34 +0800

    去掉了iOS9的storyboardreference

---
+++ DouYuZB/DouYuZB/Classes/Main/MainViewController.swift
@@ -0,0 +1,27 @@
+//
+//  MainViewController.swift
+//  DouYuZB
+//
+//  Created by 刘小二 on 2017/1/11.
+//  Copyright © 2017年 刘小二. All rights reserved.
+//
+
+import UIKit
+
+class MainViewController: UITabBarController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        addChildViewController("Home")
+        addChildViewController("Live")
+        addChildViewController("Follow")
+        addChildViewController("Profile")
+    }
+
+    private  func addChildViewController(_ storyName: String) {
+        let childVC = UIStoryboard(name: storyName, bundle: nil).instantiateInitialViewController()!
+        addChildViewController(childVC);
+    }
+
+}
